d1dd5bbfac3686652b34b046de8ab9b1b202b242
alfred becker <alfred.becker0905@gmail.com>
2021-10-30 19:07:01 +0200

    - refactor file reader - new function read in private - update test notebook

--- src/dv_reader.py
+++ src/dv_reader.py
@@ -51,13 +51,27 @@ class Data_Volley_Parser():
 
 
 
-    def create_raw_pd_file(self):
-        pass
+    def __create_raw_pd_file(self, file_path):
+        try:
+            file_linedata = open(file_path,"r", encoding="Latin-1").readlines()
+            self.__pd_file = pd.DataFrame(file_linedata, columns=[self.__col_name])
+            self.__pd_file[self.__col_name] = self.__pd_file[self.__col_name].str.replace("\n","")
+            self.__keylist_in_file = self.__pd_file[self.__pd_file[self.__col_name].str.startswith("[")]
+        except Exception as file_reader_error:
+            print("ERROR::can't readout dv-file:" + str(file_reader_error))
+            self.__pd_file = pd.DataFrame()
+            self.__keylist_in_file = []
 
 
 
 
     def get_keyword_list(self):
+        """get_keyword_list
+            * shows all keys inside the dv-files
+
+        Returns:
+            keyword list
+        """
         return self.__keyword_list
 
 
@@ -73,34 +87,34 @@ class Data_Volley_Parser():
             * create a new public variable: "file_struct"
         Args:
             file_path (glob path): path to your data volley file
-        """
-        file_linedata = open(file_path,"r", encoding="Latin-1").readlines()
-        pd_file = pd.DataFrame(file_linedata, columns=["filedata"])
-        pd_file["filedata"] = pd_file["filedata"].str.replace("\n","")
-        keylist_in_file = pd_file[pd_file["filedata"].str.startswith("[")]
+        """        
+        self.__create_raw_pd_file(file_path=file_path)
         self.file_struct = self.__dv_protocol
 
-        # set start_ids where we find the keyword inside the file
-        for entry in keylist_in_file["filedata"]:
-            self.file_struct[entry]["start_id"] = keylist_in_file[keylist_in_file["filedata"] == entry].index[0]
-        
-        # prepare file IDs
-        last_keyname = None
-        key_counter = 0
-        for key in self.file_struct:
-            key_counter += 1
-            if last_keyname != None:
-                self.file_struct[last_keyname]["end_id"] = self.file_struct[key]["start_id"] - 1
-            if key_counter == len(self.file_struct):
-                self.file_struct[key]["end_id"] = len(pd_file) - 1
-            last_keyname = key
+        if len(self.__keylist_in_file) > 0 and len(self.__pd_file) > 0:
+            # set start_ids where we find the keyword inside the file
+            for entry in self.__keylist_in_file[self.__col_name]:
+                self.file_struct[entry]["start_id"] = self.__keylist_in_file[self.__keylist_in_file[self.__col_name] == entry].index[0]
+            
+            # prepare file IDs
+            last_keyname = None
+            key_counter = 0
+            for key in self.file_struct:
+                key_counter += 1
+                if last_keyname != None:
+                    self.file_struct[last_keyname]["end_id"] = self.file_struct[key]["start_id"] - 1
+                if key_counter == len(self.file_struct):
+                    self.file_struct[key]["end_id"] = len(self.__pd_file) - 1
+                last_keyname = key
 
-        # collect data lines
-        for key in self.file_struct:
-            if self.file_struct[key]["start_id"] == self.file_struct[key]["end_id"]:
-                self.file_struct[key]["data"] = []
-            else:
-                self.file_struct[key]["data"] = list(pd_file.loc[self.file_struct[key]["start_id"]+1:self.file_struct[key]["end_id"]]["filedata"]) 
+            # collect data lines
+            for key in self.file_struct:
+                if self.file_struct[key]["start_id"] == self.file_struct[key]["end_id"]:
+                    self.file_struct[key]["data"] = []
+                else:
+                    self.file_struct[key]["data"] = list(self.__pd_file.loc[self.file_struct[key]["start_id"]+1:self.file_struct[key]["end_id"]][self.__col_name]) 
+        else:
+            print("file or keys are not guilty")
 
 
 
@@ -124,16 +138,11 @@ class Data_Volley_Parser():
         """check keywords in dv-file
             * readout dv-file and check which keywords are inside the file
             * all keywords are necessary for a correct work inside data volley application
-
         Args:
             file (glob path): path to a dv-file
         """
-        check_file = file
         try:
-            file_linedata = open(check_file,"r", encoding="Latin-1").readlines()
-            pd_linedata = pd.DataFrame(file_linedata, columns=[self.__col_name])
-            pd_linedata[self.__col_name] = pd_linedata[self.__col_name].str.replace("\n","")
-            keywords_in_scout = pd_linedata[pd_linedata[self.__col_name].str.startswith("[")]
-            self.__check_keyword_list(new_keywords=keywords_in_scout)
+            self.__create_raw_pd_file(file_path=file)
+            self.__check_keyword_list(new_keywords=self.__keylist_in_file)
         except Exception as reader_error:
             print(str(reader_error))
